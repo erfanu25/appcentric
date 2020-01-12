5017d5256ff75211784055f7da63536008343102
Erfan <mislbderfan74>
2020-01-12 18:45:51 +0600

    Initiate Project

---
+++ src/main/java/org/erfan/erfanu25/Erfanu25Application.java
@@ -0,0 +1,13 @@
+package org.erfan.erfanu25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Erfanu25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Erfanu25Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/erfan/erfanu25/Erfanu25ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.erfan.erfanu25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Erfanu25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
