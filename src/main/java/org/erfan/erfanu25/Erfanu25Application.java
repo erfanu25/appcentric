bf1f39a4587db7f942423b5704ad1e73dacb6492
Erfan <mislbderfan74>
2020-01-14 17:04:58 +0600

    View resolver & template engine

--- src/main/java/org/erfan/erfanu25/Erfanu25Application.java
+++ src/main/java/org/erfan/erfanu25/Erfanu25Application.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@EnableSwagger2
+//@Import(SpringDataRestConfiguration.class)
 public class Erfanu25Application {
 
 	public static void main(String[] args) {
---
+++ src/main/java/org/erfan/erfanu25/controller/HelloController.java
@@ -0,0 +1,15 @@
+package org.erfan.erfanu25.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController {
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String printHello(ModelMap model) {
+        model.addAttribute("message", "Hello Spring MVC Framework!");
+        return "hello";
+    }
+}
